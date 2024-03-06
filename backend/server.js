a68f00a86c26287269fb31907fcae4c99a465661
KrishnaCJ <kriishcj@gmail.com>
2024-03-06 15:24:45 +0530

    last changes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,21 +10,26 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
-app.use(express.json()); //to parse the incoming request with JSON Payloads(from req.body)
-app.use(cookieParser()); //to parse the incoming request with cookie
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-// app.get("/", (req, res) => {
-//   res.send("Hey Buddy ready for Chat");
-// });
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
-  console.log(`server is running on  port ${8000}`);
+  console.log(`Server Running on port ${PORT}`);
 });
