7c4466794494175564ad06b5774445aef9c6920c
KrishnaCJ <kriishcj@gmail.com>
2024-03-06 16:37:40 +0530

    fixed message right part

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,61 +1,70 @@
-import { useState } from "react";
-import { BsSend } from "react-icons/bs";
-import useSendMessage from "../../hooks/useSendMessage";
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
+import MessageInput from "./MessageInput";
+import Messages from "./Messages";
+import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from "../../context/AuthContext";
 
-const MessageInput = () => {
-  const [message, setMessage] = useState("");
-  const { loading, sendMessage } = useSendMessage();
+const MessageContainer = () => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!message) return;
-    await sendMessage(message);
-    setMessage("");
-  };
+  useEffect(() => {
+    // cleanup function (unmounts)
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
 
   return (
-    <form className="px-4 my-3" onSubmit={handleSubmit}>
-      <div className="w-full relative">
-        <input
-          type="text"
-          className="border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white"
-          placeholder="Send a message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="absolute inset-y-0 end-0 flex items-center pe-3"
-        >
-          {loading ? (
-            <div className="loading loading-spinner"></div>
-          ) : (
-            <BsSend />
-          )}
-        </button>
-      </div>
-    </form>
+    <div className="md:min-w-[450px] flex flex-col">
+      {!selectedConversation ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          {/* Header */}
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text">To:</span>{" "}
+            <span className="text-gray-900 font-bold">
+              {selectedConversation.fullName}
+            </span>
+          </div>
+          <Messages />
+          <MessageInput />
+        </>
+      )}
+    </div>
   );
 };
-export default MessageInput;
+export default MessageContainer;
+
+const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+        <p>Welcome 👋 {authUser.fullName} ❄</p>
+        <p>Select a chat to start messaging</p>
+        <TiMessages className="text-3xl md:text-6xl text-center" />
+      </div>
+    </div>
+  );
+};
 
 // STARTER CODE SNIPPET
-// import { BsSend } from "react-icons/bs";
+// import MessageInput from "./MessageInput";
+// import Messages from "./Messages";
 
-// const MessageInput = () => {
+// const MessageContainer = () => {
 // 	return (
-// 		<form className='px-4 my-3'>
-// 			<div className='w-full'>
-// 				<input
-// 					type='text'
-// 					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
-// 					placeholder='Send a message'
-// 				/>
-// 				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
-// 					<BsSend />
-// 				</button>
-// 			</div>
-// 		</form>
+// 		<div className='md:min-w-[450px] flex flex-col'>
+// 			<>
+// 				{/* Header */}
+// 				<div className='bg-slate-500 px-4 py-2 mb-2'>
+// 					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
+// 				</div>
+
+// 				<Messages />
+// 				<MessageInput />
+// 			</>
+// 		</div>
 // 	);
 // };
-// export default MessageInput;
+// export default MessageContainer;
