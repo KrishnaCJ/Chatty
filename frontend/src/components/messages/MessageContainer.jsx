d7f139063a2539ef2c37768154eebe2f0f1309c6
KrishnaCJ <kriishcj@gmail.com>
2024-03-06 15:10:20 +0530

    All set

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,36 +1,61 @@
-import { useAuthContext } from "../../context/AuthContext";
-import { extractTime } from "../../utils/extractTime";
-import useConversation from "../../zustand/useConversation";
+import { useState } from "react";
+import { BsSend } from "react-icons/bs";
+import useSendMessage from "../../hooks/useSendMessage";
 
-const Message = ({ message }) => {
-  const { authUser } = useAuthContext();
-  const { selectedConversation } = useConversation();
-  const fromMe = message.senderId === authUser._id;
-  const formattedTime = extractTime(message.createdAt);
-  const chatClassName = fromMe ? "chat-end" : "chat-start";
-  const profilePic = fromMe
-    ? authUser.profilePic
-    : selectedConversation?.profilePic;
-  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+const MessageInput = () => {
+  const [message, setMessage] = useState("");
+  const { loading, sendMessage } = useSendMessage();
 
-  const shakeClass = message.shouldShake ? "shake" : "";
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!message) return;
+    await sendMessage(message);
+    setMessage("");
+  };
 
   return (
-    <div className={`chat ${chatClassName}`}>
-      <div className="chat-image avatar">
-        <div className="w-10 rounded-full">
-          <img alt="Tailwind CSS chat bubble component" src={profilePic} />
-        </div>
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
+      <div className="w-full relative">
+        <input
+          type="text"
+          className="border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white"
+          placeholder="Send a message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="absolute inset-y-0 end-0 flex items-center pe-3"
+        >
+          {loading ? (
+            <div className="loading loading-spinner"></div>
+          ) : (
+            <BsSend />
+          )}
+        </button>
       </div>
-      <div
-        className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}
-      >
-        {message.message}
-      </div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-        {formattedTime}
-      </div>
-    </div>
+    </form>
   );
 };
-export default Message;
+export default MessageInput;
+
+// STARTER CODE SNIPPET
+// import { BsSend } from "react-icons/bs";
+
+// const MessageInput = () => {
+// 	return (
+// 		<form className='px-4 my-3'>
+// 			<div className='w-full'>
+// 				<input
+// 					type='text'
+// 					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
+// 					placeholder='Send a message'
+// 				/>
+// 				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
+// 					<BsSend />
+// 				</button>
+// 			</div>
+// 		</form>
+// 	);
+// };
+// export default MessageInput;
